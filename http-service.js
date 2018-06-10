b5b170556d23154317bf30b048e965e82b808f2e
François Daoust <fd@tidoust.net>
2018-06-10 14:59:02 +0200

    Add a query cache and keep crawl reports in memory
    Crawling a spec takes time (typically a few seconds). To make sure the service
    only crawls specs when needed, a new cache was added at the query level for
    POST requests that send the HTML contents of the specs to crawl.
    
    Note this cache is restricted to POST requests that contain the HTML of the
    specs to crawl for now: in the absence of HTML content, it is hard to tell
    whether the crawl result we have in cache is still valid (the HTML content of
    the spec may have changed in the meantime) without re-fetching the spec.
    
    Cache entries are periodically refreshed (once per day by default), to account
    for indirect updates that do not touch on the HTML content itself (e.g. updates
    to scripts that the spec depends on).
    
    Main crawl reports (from reffy-reports) are now also kept in memory to avoid
    having to parse a lot of JSON at each request. This saves ~70ms per request.

--- http-service.js
+++ http-service.js
@@ -10,6 +10,8 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const rimraf = require('rimraf');
 const URL = require('whatwg-url').URL;
 const crawlList = require('reffy/src/cli/crawl-specs').crawlList;
 const mergeCrawlResults = require('reffy/src/cli/merge-crawl-results').mergeCrawlResults;
@@ -72,6 +74,41 @@ config.cacheRefresh = config.cacheRefresh || 'force';
  */
 config.cacheFolder = config.cacheFolder || '.cache';
 
+
+/**
+ * Query cache folder (contains results of individual crawls)
+ *
+ * @type {[type]}
+ */
+config.queryCacheFolder = config.queryCacheFolder || '.querycache';
+
+/**
+ * Cache refresh strategy for query checks. Defaults to refreshing the contents
+ * of the cache after one day. Possible values are "force" (which means the
+ * cache will never be used), "never" (which means cache entries will always
+ * be considered to be valid), or an integer (number of seconds before
+ * expiration).
+ *
+ * Note the request may set a "refresh" parameter to "force" to force the
+ * service to redo a crawl. This can be useful when the spec has not changed
+ * but external scripts it uses have (e.g. ReSpec).
+ */
+config.queryCacheRefresh = config.queryCacheRefresh || 86400;
+
+
+/**
+ * Maximum number of entries to keep in the query cache folder
+ *
+ * Note that is a soft limit, meaning that the actual number of entries may
+ * slightly exceed that number while the garbage collector does its job.
+ *
+ * Set the parameter to -1 to disable the garbage collector entirely.
+ *
+ * @type {Number}
+ */
+config.queryCacheMaxEntries = config.queryCacheMaxEntries || 10000;
+
+
 /**
  * Whether to output additional execution traces to the console
  * (should only be set in debug mode)
@@ -93,6 +130,47 @@ ParamError.prototype = Error.prototype;
  * HTTP request identifier (incremented after each request)
  */
 let counter = 0;
+
+
+/**
+ * Number of entries in the query cache
+ */
+let inQueryCache = 0;
+
+
+/**
+ * Crawl reports in memory
+ */
+let crawlReports = {};
+
+
+/**
+ * Make sure that the query cache folder exists and is empty
+ */
+async function checkQueryCacheFolder() {
+  try {
+    let stat = await fs.stat(config.queryCacheFolder);
+    if (!stat.isDirectory()) {
+      throw new Error('Looking for a query cache folder but found a cache file instead');
+    }
+    rimraf.sync(config.queryCacheFolder + '/*');
+  }
+  catch (err) {
+    // Create the folder if it does not exist yet
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+    try {
+      await fs.mkdir(config.queryCacheFolder);
+    }
+    catch (mkerr) {
+      // Someone may have created the folder in the meantime
+      if (mkerr.code !== 'EEXIST') {
+        throw mkerr;
+      }
+    }
+  }
+}
 
 
 
@@ -158,19 +236,21 @@ function requireFromWorkingDirectory(filename) {
  * @param  {Request} req HTTP request
  * @return {Promise(Object)} Promise to get query parameters
  */
-async function getQueryFromRequest(req) {
+async function prepareQueryFromHttpRequest(req) {
   if (req.method === 'GET') {
     return {
       specs: ((req.query.urls || req.query.url) ? (req.query.urls || req.query.url).split(',') : []).map(url => { return { url }; }),
       crawl: req.query.crawl,
-      report: (req.query.report ? req.query.report.split(',') : ['analysis'])
+      report: (req.query.report ? req.query.report.split(',') : ['analysis']),
+      refresh: req.query.refresh
     };
   }
   else if (req.body && req.body.specs) {
     return {
       specs: req.body.specs,
       crawl: req.body.crawl,
-      report: (req.body.report ? (Array.isArray(req.body.report) ? req.body.report : req.body.report.split(',')) : ['analysis'])
+      report: (req.body.report ? (Array.isArray(req.body.report) ? req.body.report : req.body.report.split(',')) : ['analysis']),
+      refresh: req.body.refresh
     };
   }
   else {
@@ -180,7 +260,8 @@ async function getQueryFromRequest(req) {
         html: req.body.html
       }],
       crawl: req.body.crawl,
-      report: (req.body.report ? (Array.isArray(req.body.report) ? req.body.report : req.body.report.split(',')) : ['analysis'])
+      report: (req.body.report ? (Array.isArray(req.body.report) ? req.body.report : req.body.report.split(',')) : ['analysis']),
+      refresh: req.body.refresh
     };
   }
 }
@@ -235,6 +316,9 @@ async function validateQuery(query) {
   }
   else if (query.report.some(report => !['all', 'idl', 'crawl', 'analysis'].includes(report))) {
     throw new ParamError('The "report" parameter must be one (or more) of "idl", "crawl", or "analysis".');
+  }
+  else if (query.refresh && (query.refresh !== 'force')) {
+    throw new ParamError('The "refresh" parameter can only be set to "force".');
   }
   else {
     query.crawl = query.crawl || 'w3c';
@@ -333,14 +417,27 @@ async function assembleResponse(query, crawl, study) {
 
 
 /**
- * Fetches the requested report from the network or from local file storage
+ * Fetches the requested report from memory storage, local file storage or
+ * the network (with a refresh strategy based on the "crawlRefresh" parameter)
  *
  * @param  {String} crawlType  The crawl type ("w3c", "w3c-tr", "whatwg")
  * @param  {String} reportType The report to retrieve ("crawl" or "study")
  * @return {Promise(Object)} The promise to get the report's results
  */
 async function fetchReport(crawlType, reportType) {
-  let res = [];
+  let res = (crawlReports[crawlType] || {})[reportType];
+  let now = Date.now();
+
+  if (res && ((config.crawlRefresh === 'force') ||
+    (Number.isInteger(config.crawlRefresh) &&
+      (now - res.time > config.crawlRefresh * 1000)))) {
+    res = null;
+  }
+
+  if (res) {
+    return res.results;
+  }
+
   if (config.crawlReports.startsWith('http:') ||
       config.crawlReports.startsWith('https:')) {
     let response = await fetch(
@@ -353,7 +450,149 @@ async function fetchReport(crawlType, reportType) {
     res = requireFromWorkingDirectory(
       path.resolve(config.crawlReports, crawlType, `${reportType}.json`));
   }
+
+  crawlReports[crawlType] = crawlReports[crawlType] || {};
+  crawlReports[crawlType][reportType] = {
+    time: now,
+    results: res.results || []
+  };
   return res.results || [];
+}
+
+
+/**
+ * Flag set when the garbage collector is running
+ *
+ * @type {Boolean}
+ */
+let isGarbageCollectorRunning = false;
+
+
+/**
+ * Run the garbage collector on the query cache to make some room.
+ *
+ * @return {[type]} [description]
+ */
+async function garbageCollectQueryCache() {
+  // Only run GC once at a time
+  if (isGarbageCollectorRunning) {
+    return;
+  }
+  isGarbageCollectorRunning = true;
+
+  const files = await fs.readdir(config.queryCacheFolder);
+  const stats = await Promise.all(files.map(async file => {
+    const name = path.join(config.queryCacheFolder, file);
+    try {
+      const stat = await fs.stat(name);
+      if (stat.isDirectory()) {
+        return null;
+      }
+      return { name, atime: stat.atimeMs };
+    }
+    catch (err) {
+      // File may have been dropped one way or the other
+      return null;
+    }
+  }));
+
+  // Sort files to put oldest accessed ones first
+  const orderedStats = stats
+    .filter(stat => !!stat)
+    .sort((file1, file2) => file1.atime - file2.atime);
+
+  // Remove 1/4 of entries in the query cache folder to make some room
+  const maxToRemove = config.queryCacheMaxEntries / 4;
+  let removed = 0;
+  for (stat of orderedStats) {
+    if (removed > config.queryCacheMaxEntries / 4) {
+      break;
+    }
+    removed += 1;
+    inQueryCache -= 1;
+    try {
+      await fs.unlink(stat.name);
+    }
+    catch(err) {
+      console.warn(`Could not delete file ${stat.name} from cache`);
+    }
+  }
+
+  isGarbageCollectorRunning = false;
+}
+
+
+/**
+ * Search the crawl results in the query cache
+ *
+ * Note the query must contain the HTML of requested specs, otherwise we won't
+ * use the cache (because there is no easy way to tell whether the contents of
+ * the spec wouldn't have changed in the meantime)
+ *
+ * @param  {Object} query The query to search
+ * @return {Promise<Object>} The promise to get crawl results from the query
+ *   cache, or null if the results of the crawl are not in the query cache
+ */
+async function fetchCrawlResultsFromQueryCache(query) {
+  if (query.specs.find(spec => !spec.html)) {
+    return null;
+  }
+  if ((config.queryCacheRefresh === 'force') || (query.refresh === 'force')) {
+    return null;
+  }
+
+  let hash = crypto.createHash('md5')
+    .update(JSON.stringify(query.specs), 'utf8')
+    .digest('hex');
+  let cacheFilename = path.join(config.queryCacheFolder, hash + '.json');
+  try {
+    let now = Date.now();
+    let stat = await fs.stat(cacheFilename);
+    if (Number.isInteger(config.queryCacheRefresh) &&
+        (now - stat.mtimeMs > config.queryCacheRefresh * 1000)) {
+      return null;
+    }
+
+    let data = await fs.readFile(cacheFilename);
+    let obj = JSON.parse(data, 'utf8');
+    return obj;
+  }
+  catch (err) {
+    return null;
+  }
+}
+
+
+/**
+ * Save the crawl results to the query cache
+ *
+ * See note in fetchCrawlResultsFromQueryCache about the need to have the HTML
+ * of requested specs.
+ *
+ * @param  {Object} query The query for which we want to save results
+ * @param  {Object} results The crawl results to save
+ * @return {Promise} The promise to have saved the result (resolved when
+ *   function is over, regardless of whether save was successful)
+ */
+async function saveCrawlResultsToQueryCache(query, results) {
+  if (query.specs.find(spec => !spec.html)) {
+    return;
+  }
+
+  // Time to run the garbage collector?
+  // (will be done in parallel)
+  inQueryCache += 1;
+  if ((config.queryCacheMaxEntries >= 0) &&
+      (inQueryCache > config.queryCacheMaxEntries)) {
+    garbageCollectQueryCache();
+  }
+
+  let hash = crypto.createHash('md5')
+    .update(JSON.stringify(query.specs), 'utf8')
+    .digest('hex');
+  let cacheFilename = path.join(config.queryCacheFolder, hash + '.json');
+  await fs.writeFile(cacheFilename, JSON.stringify(results, null, 2), 'utf8');
+  return;
 }
 
 
@@ -365,11 +604,20 @@ async function fetchReport(crawlType, reportType) {
  * @return {Promise(Object)} The promise to get a freshly baked anomalies report
  * for the specs in the query
  */
-async function processCheckQuery(query) {
+async function processCheckQuery(query, requestId) {
   // Load latest crawl report
+  // console.time(`${requestId} - fetchReport`);
   let refCrawl = await fetchReport(query.crawl, 'crawl');
+  // console.timeEnd(`${requestId} - fetchReport`);
+  // console.time(`${requestId} - crawlList`);
   let crawlOptions = { publishedVersion: (query.crawl === 'w3c-tr') };
-  let crawlResults = await crawlList(query.specs, crawlOptions)
+  let crawlResults = await fetchCrawlResultsFromQueryCache(query);
+  if (!crawlResults) {
+    crawlResults = await crawlList(query.specs, crawlOptions);
+    saveCrawlResultsToQueryCache(query, crawlResults);
+  }
+  // console.timeEnd(`${requestId} - crawlList`);
+  // console.time(`${requestId} - mergeCrawlResults`);
   let crawl = {
     type: 'crawl',
     title: 'Anomalies in spec: ' + query.specs.map(prop('url')).join(', '),
@@ -383,7 +631,10 @@ async function processCheckQuery(query) {
     results: crawlResults
   };
   let mergedCrawl = await mergeCrawlResults(crawl, refCrawl);
+  // console.timeEnd(`${requestId} - mergeCrawlResults`);
+  // console.time(`${requestId} - studyCrawl`);
   let study = studyCrawl(mergedCrawl, query.specs);
+  // console.timeEnd(`${requestId} - studyCrawl`);
 
   return assembleResponse(query, mergedCrawl.results, study.results);
 }
@@ -427,15 +678,15 @@ function getRequestMiddleware(processFunction) {
   return (req, res, next) => {
     const requestId = counter;
     counter += 1;
-    //console.time(`request ${requestId}`);
-    getQueryFromRequest(req)
+    // console.time(`request ${requestId}`);
+    prepareQueryFromHttpRequest(req)
       .then(validateQuery)
-      .then(processFunction)
+      .then(query => processFunction(query, requestId))
       .then(result => Object.assign({ status: 'ok' }, result))
       .then(result => res.json(result))
       .then(_ => next())
       .catch(err => next(err));
-      //.then(_ => console.timeEnd(`request ${requestId}`));
+      // .then(_ => console.timeEnd(`request ${requestId}`));
   };
 }
 
@@ -449,7 +700,7 @@ app.set('json spaces', 2);
 monitor.install(app, { entries: config.entries });
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' }));
 
 
 ///////////////////////////
@@ -505,10 +756,14 @@ fetch.setParameter('refresh', config.cacheRefresh);
 // Start the HTTP server
 ///////////////////////////
 
-app.listen(config.httpPort, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(64);
-  }
-  console.log('Started');
-});
+(async _ => {
+  await checkQueryCacheFolder();
+
+  app.listen(config.httpPort, (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(64);
+    }
+    console.log('Started');
+  });
+})();
